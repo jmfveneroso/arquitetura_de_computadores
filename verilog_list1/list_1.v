b7058056ee2ae643690b2917c5906defb7463635
jmfveneroso <jmfveneroso@gmail.com>
2017-10-05 13:47:23 -0300

    Lista 1 de Verilog.

---
+++ verilog_list1/list_1.v
@@ -0,0 +1,10 @@
+`timescale 10ps / 1ps
+module list_1(input input_tmp, output output_tmp);
+
+exercise4tb e4tb ();
+exercise5tb e5tb ();
+exercise6tb e6tb ();
+exercise7tb e7tb ();
+alutb alu1tb ();
+
+endmodule 
