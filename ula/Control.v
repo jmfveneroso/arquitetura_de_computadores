7a5dbbeb4a6742c36e9edc035568666742f690f2
João Mateus de Freitas Veneroso <jmfveneroso@gmail.com>
2017-10-09 09:12:01 -0300

    Mudanças do Iuri.

---
+++ ula/Control.v
@@ -0,0 +1,62 @@
+//							Iuri Silva Castro
+//			Mestrando em Engenharia Eletrica - PPGEE
+//			Universidade Federal de Minas Gerais - UFMG
+//									2º/2017
+
+/* Controle da microprocessador */
+module Control (CLK, RST, Start, Ready, Wen );
+	
+	input CLK, RST, Start;
+	output reg Ready, Wen;
+	
+	reg [2:0] State;
+	
+	parameter S0 = 3'b000, S1 = 3'b001, S2 = 3'b010, S3 = 3'b011, S4 = 3'b100, S5 = 3'b101;
+	
+	always @(posedge CLK) begin
+		if ( RST ) begin
+			State <= S0;		// Inicia em Halt
+			Ready <= 1;
+			Wen <= 0;
+		end
+		else begin
+			case (State)
+				S0:						// Halt, aguarda sinal de 'Start'
+				begin
+					Ready <= 1;
+					Wen <= 0;
+					if (Start) begin
+						State <= S1;
+					end
+				end
+				S1:						// Decodificaçao de instruçao
+				begin
+					Ready <= 0;
+					Wen <= 0;
+					State <= S2;
+				end
+				S2:						// Busca de valores nos registradores
+				begin
+					Ready <= 0;
+					Wen <= 0;
+					State <= S3;
+				end
+				S3: 						// Execuçao 
+				begin
+					Ready <= 0;
+					Wen <= 0;
+					State <= S4;
+				end
+				S4:						// Write Back 
+				begin
+					Ready <= 0;
+					Wen <= 1;
+					State <= S0;
+				end
+				default: begin
+					State = S0;
+				end
+			endcase
+		end
+	end	
+endmodule
---
+++ ula/ULA.v
@@ -0,0 +1,132 @@
+//							Iuri Silva Castro
+//			Mestrando em Engenharia Eletrica - PPGEE
+//			Universidade Federal de Minas Gerais - UFMG
+//									2º/2017
+
+/* Codigos das instruçoes:
+			ADD = 0000
+			SUB = 0001
+			SLT = 0010
+			AND = 0011
+			OR =  0100
+			XOR = 0101
+*/
+
+// OpA, OpB: Operandos de entrada. 16-bit
+// Res: Resultado da operaçao. 16-bit
+// Op: Codigo da operaçao a ser executada
+module ULA (OpA, OpB, Res, Op, FlagReg, CLK, RST);
+
+	input CLK, RST;
+	input [3:0] Op;
+	input [15:0] OpA, OpB;
+	output reg [15:0] Res;
+	output reg [2:0] FlagReg;		// [Z N C]: Z=Zero; N=Neg; C=Carry/Overflow
+	
+	wire [15:0] invOpB;
+	assign invOpB = ~OpB + 16'd1;
+
+	parameter InsADD = 4'b0000;	// ADD	Res = OpA + OpB
+	parameter InsSUB = 4'b0001;	//	SUB	Res = OpA - OpB
+	parameter InsSLT = 4'b0010;	//	SLTI	Res = (OpA > OpB) ? 1 : 0
+	parameter InsAND = 4'b0011;	//	AND 	Res = OpA & OpB
+	parameter InsOR =  4'b0100;	//	OR 	Res = OpA | OpB
+	parameter InsXOR = 4'b0101;	//	XOR 	Res = OpA ^ OpB
+	
+	parameter OverflowFlag	= 0;
+	parameter NegFlag 		= 1;
+	parameter ZeroFlag		= 2;
+	
+	always @(posedge CLK) begin
+		if (RST) begin
+				FlagReg = 3'b000;
+		end
+		else begin
+			case (Op)
+				InsADD: begin			
+					Res = OpA + OpB;
+					
+					/* Zero check */
+					if (Res == 0)
+						FlagReg[ZeroFlag] = 1'b1;
+					else
+						FlagReg[ZeroFlag] = 1'b0;
+					/* Overflow check */
+					FlagReg[OverflowFlag] = (OpA[15] & OpB[15] & ~Res[15]) | (~OpA[15] & ~OpB[15] & Res[15]);
+					/* Negative check */
+					FlagReg[NegFlag] = Res[15];
+				end
+				InsSUB: begin			
+					Res = OpA + invOpB;
+					
+					/* Zero check */
+					if (Res == 0)
+						FlagReg[ZeroFlag] = 1'b1;
+					else
+						FlagReg[ZeroFlag] = 1'b0;
+					/* Overflow check */
+					FlagReg[OverflowFlag] = (OpA[15] & invOpB[15] & ~Res[15]) | (~OpA[15] & ~invOpB[15] & Res[15]);
+					/* Negative check */
+					FlagReg[NegFlag] = Res[15];
+				end
+				InsSLT: begin
+					if (OpA > OpB)
+						Res = 16'd1;
+					else
+						Res = 16'd0;
+						
+					/* Zero check */
+					if (Res == 0)
+						FlagReg[ZeroFlag] = 1'b1;
+					else
+						FlagReg[ZeroFlag] = 1'b0;
+					/* Overflow check */
+					FlagReg[OverflowFlag] = (OpA[15] & OpB[15] & ~Res[15]) | (~OpA[15] & ~OpB[15] & Res[15]);
+					/* Negative check */
+					FlagReg[NegFlag] = Res[15];
+				end
+				InsAND: begin			
+					Res = OpA & OpB;
+					
+					/* Zero check */
+					if (Res == 0)
+						FlagReg[ZeroFlag] = 1'b1;
+					else
+						FlagReg[ZeroFlag] = 1'b0;
+					/* Overflow check */
+					FlagReg[OverflowFlag] = (OpA[15] & OpB[15] & ~Res[15]) | (~OpA[15] & ~OpB[15] & Res[15]);
+					/* Negative check */
+					FlagReg[NegFlag] = Res[15];
+				end
+				InsOR: begin			
+					Res = OpA | OpB;
+					
+					/* Zero check */
+					if (Res == 0)
+						FlagReg[ZeroFlag] = 1'b1;
+					else
+						FlagReg[ZeroFlag] = 1'b0;
+					/* Overflow check */
+					FlagReg[OverflowFlag] = (OpA[15] & OpB[15] & ~Res[15]) | (~OpA[15] & ~OpB[15] & Res[15]);
+					/* Negative check */
+					FlagReg[NegFlag] = Res[15];
+				end
+				InsXOR: begin			
+					Res = OpA ^ OpB;
+					
+					/* Zero check */
+					if (Res == 0)
+						FlagReg[ZeroFlag] = 1'b1;
+					else
+						FlagReg[ZeroFlag] = 1'b0;
+					/* Overflow check */
+					FlagReg[OverflowFlag] = (OpA[15] & OpB[15] & ~Res[15]) | (~OpA[15] & ~OpB[15] & Res[15]);
+					/* Negative check */
+					FlagReg[NegFlag] = Res[15];
+				end
+			endcase
+		end
+	end
+endmodule
+
+
